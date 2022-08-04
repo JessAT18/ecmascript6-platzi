a5266528c8c45c864df6694cf5c000ebb64b2cb6
JessAT18 <j-aquino@outlook.com>
2022-08-04 14:16:40 -0400

    Let, const, multilinea, spread operator y desestructuracion.

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,5 @@
+//Clase 2
+
 function newFunc(name, age, country) {
     var name = name || 'Jessica';
     var age = age || 22;
@@ -14,7 +16,7 @@ function newFunction(name = 'Jessica', age = 22, country = 'BO') {
 newFunction();
 newFunction('Alice', '19', 'UK');
 
-//--------------------------------------------------------------------------
+//------------
 
 let hello = 'Hello';
 let world = 'World';
@@ -23,3 +25,61 @@ let epicPhrase = hello + ' ' + world;
 console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+//---------------------------------------------------------------------------
+//Clase 3
+
+let lorem = "Hola! Estoy escribiendo algo breve para poder usar como \n"
++ "ejemplo en el curso de JS"
+
+//es6
+let lorem2 = `Otro texto que tenemos que escribir
+para poder ejemplificar el uso de los template literals`;
+
+console.log(lorem);
+console.log(lorem2);
+
+//---
+
+let person = {
+    'name': 'Jessica',
+    'age': 22,
+    'country': 'BO'
+};
+
+console.log(person.name, person.age, person.country);
+
+//es6
+
+let { name, age } = person;
+console.log(name, age);
+
+//---
+
+let team1 = ['Jessica', 'Andrea', 'Giulianne'];
+let team2 = ['Nancy', 'Rosmery', 'Paola'];
+
+let education = ['Michael', ...team1, ...team2];
+
+console.log(education);
+
+//---
+
+{
+    var globalVar = "GLOBAL VAR";
+}
+
+{
+    let globalLet = "GLOBAL LET";
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+//console.log(globalLet);   //Let no es accesible desde una instancia local
+
+//---
+
+const a = 'b';
+//a = 'a';                  //Const no nos permite modificar a
+console.log(a);
+
