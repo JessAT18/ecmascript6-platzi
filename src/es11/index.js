a099843690c18d281325cb80cd9c6143dd744e42
JessAT18 <j-aquino@outlook.com>
2022-08-05 16:20:46 -0400

    ES11 nuevas caracteristicas

--- src/es11/index.js
+++ src/es11/index.js
@@ -4,3 +4,44 @@ button.addEventListener("click", async function () {
     const module = await import("./file.js");
     module.hello();
 });
+
+//-----------------------------------------------------------
+//Clase 12
+
+const aBigNumber = 901213933223223323n;
+
+const anotherBigNumber = BigInt(901213933223223323);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
+
+//--
+
+const promise1 = new Promise((resolve, reject) => reject('reject'));
+const promise2 = new Promise((resolve, reject) => resolve('resolve'));
+const promise3 = new Promise((resolve, reject) => resolve('resolve1'));
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+//--
+console.log(window);
+console.log(self);
+console.log(frames);
+console.log(this);
+
+console.log(globalThis);
+
+//--
+const foo = null ?? 'default string';
+console.log(foo);
+
+//--
+const user = {};
+console.log(user?.profile?.email);
+
+if (user?.profile?.email) {
+    console.log('email');
+} else {
+    console.log('fail');
+}
