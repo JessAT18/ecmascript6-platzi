714c45d97a3b1c4e877ba7d75ffb4609b21896e6
JessAT18 <j-aquino@outlook.com>
2022-08-05 14:02:12 -0400

    Async await

--- src/es8/index.js
+++ src/es8/index.js
@@ -30,3 +30,34 @@ console.log('food'.padEnd(12, ' -----'));
 const obj = {
     name: 'Jessica',
 }
+
+//---------------------------------------------------------------------------
+//Clase 8
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('Hello world'), 3000)
+            : reject(new Error('Test error'));
+    });
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+//--
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
