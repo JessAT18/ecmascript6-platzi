e132a02934435f1ba7df57f79fa46bcfb6f1c42c
JessAT18 <j-aquino@outlook.com>
2022-08-10 11:27:43 -0400

    ES12

--- src/es12/index.js
+++ src/es12/index.js
@@ -24,5 +24,22 @@ const promise1 = new Promise((resolve, reject) => reject('1'));
 const promise2 = new Promise((resolve, reject) => resolve('2'));
 const promise3 = new Promise((resolve, reject) => resolve('3'));
 
-Promise.any([promise1, promise2, promise3])
+Promise.any([promise1, promise2, promise3]) //Se retorna el primer valor que se retorna
     .then(response => console.log(response));
+
+//--
+class AnyClass {
+    constructor(element)
+    //this.ref = new WeakRef(element);
+    ///{...}
+}
+
+//--
+let isTrue = true;
+let isFalse = false;
+console.log(isTrue &&= isFalse);
+
+console.log(isTrue ||= isFalse);
+
+let isUndef = undefined;
+console.log(isUndef ??= isFalse);
